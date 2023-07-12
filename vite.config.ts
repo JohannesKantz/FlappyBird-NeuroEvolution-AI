a60cc9dc335e685f105131c66cd5fa1b88842c1a
Johannes Kantz <67144859+JohannesKantz@users.noreply.github.com>
2023-07-12 09:07:27 +0200

    add: github pages deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/FlappyBird-NeuroEvolution-AI/",
     plugins: [svelte()],
     build: {
         chunkSizeWarningLimit: 5000,
