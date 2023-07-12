fa6f9a1afd95ed34575e580e25131b2d867184f3
Johannes Kantz <67144859+JohannesKantz@users.noreply.github.com>
2023-07-12 08:52:59 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [svelte()],
+    build: {
+        chunkSizeWarningLimit: 5000,
+    },
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+    },
+});
